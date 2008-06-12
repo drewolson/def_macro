530583ac2c2cc1e735535a3e45b9ab9ad1084dfd
Drew Olson <drew@drewolson.org>
2008-06-11 20:41:43 -0700

    added test for defining macros on main

--- test/test_def_macro.rb
+++ test/test_def_macro.rb
@@ -10,11 +10,24 @@ class Foo
   end
 end
 
+def_macro :with do |args,body|
+  eval args.first
+  eval body
+end
+
 class TestDefMacro < Test::Unit::TestCase
-  def test_with_macro
+  def test_with_macro_in_class_def
     foo = Foo.new
     
     result = foo.with proc { a = 1; b = 3 } do
+      a + b
+    end
+    
+    assert_equal(4, result)
+  end
+  
+  def test_with_macro_in_main
+    result = with proc { a = 1; b = 3 } do
       a + b
     end
     
